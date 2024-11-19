82f18d55ac41a1724b51f609dec62ef52e3e6724
Francemy <francemysebastiaofrancemy@gmail.com>
2024-11-19 17:05:33 +0100

    elimitar importação desnecessario

--- src/main/java/com/exemplo/usermanagement/controller/UserController.java
+++ src/main/java/com/exemplo/usermanagement/controller/UserController.java
@@ -4,13 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import com.exemplo.usermanagement.dto.JwtPayload;
 import com.exemplo.usermanagement.dto.UserDTO;
 import com.exemplo.usermanagement.dto.loginUserDTO;
 import com.exemplo.usermanagement.model.User;
